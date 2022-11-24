b4033d082aee3b45876a5c06fdf9c152b390a850
Sheikh Aminul <aminulfse@gmail.com>
2022-11-24 20:03:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+i18next-lite<br>
+[![NPM Version](https://img.shields.io/npm/v/i18next-lite.svg?branch=main)](https://www.npmjs.com/package/i18next-lite)
+[![Install Size](https://badgen.net/packagephobia/install/i18next-lite)](https://packagephobia.now.sh/result?p=i18next-lite)
+[![Downloads Per Week](https://img.shields.io/npm/dw/i18next-lite)](https://www.npmjs.com/package/i18next-lite)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/SheikhAminul/i18next-lite/blob/main/LICENSE)
+================
---
+++ src/index.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react'
+import { createContext, ReactElement, useContext, useState } from 'react'
+
+type Translations = {
+    [language: string]: {
+        translation: {
+            [key: string]: string
+        }
+    }
+}
+type TranslatorConfiguration = { language?: string, defaultLanguage?: string, translations?: Translations }
+type TranslateFunction = (key: string, substitutions?: any) => string
+type ConfigureFunction = ({ language, defaultLanguage, translations }: TranslatorConfiguration) => void
+
+const TranslationContext = createContext({} as {
+    configure: ConfigureFunction,
+    translate: TranslateFunction
+})
+
+function TranslationProvider({ language, defaultLanguage, translations, children }: { language?: string, defaultLanguage?: string, translations: Translations, children: ReactElement }) {
+    function validateConfiguration({ language, defaultLanguage, translations }: TranslatorConfiguration) {
+        if (!language && translations) {
+            language = navigator.language
+            if (!translations[language]) language = language.substring(0, 2)
+            if (!translations[language]) language = navigator.languages.find(language => translations[language]) as string
+            if (!translations[language]) language = translations[defaultLanguage as string] ? defaultLanguage : Object.keys(translations)[0]
+        }
+        return { language, defaultLanguage, translations } as { translations: Translations, language: string, defaultLanguage?: string }
+    }
+
+    const [configuration, setConfiguration] = useState(
+        validateConfiguration({
+            translations,
+            ...(language ? { language } : {}),
+            ...(defaultLanguage ? { defaultLanguage } : {})
+        })
+    )
+
+    const { translation: activeTranslation } = configuration.translations[configuration.language]
+
+    function configure(translatorConfiguration: TranslatorConfiguration) {
+        setConfiguration(
+            validateConfiguration({
+                ...configuration,
+                ...translatorConfiguration
+            })
+        )
+    }
+
+    function translate(key: string, substitutions?: any) {
+        let translation = activeTranslation[key]
+        if (!translation) console.error(`The key "${key}" was not found!`)
+        return translation?.replace(/{{[^{}]+}}/, (substitution: string) => substitutions[substitution.slice(2, -2)]) as string
+    }
+
+    return (
+        <TranslationContext.Provider value={{ configure, translate }}   >
+            {children}
+        </TranslationContext.Provider>
+    )
+}
+
+function useTranslate() {
+    const { translate } = useContext(TranslationContext) || {}
+    if (!translate) throw new Error('useTranslate must be used within TranslationProvider.')
+    return translate as TranslateFunction
+}
+
+function useTranslatorConfigurer() {
+    const { configure } = useContext(TranslationContext) || {}
+    if (!configure) throw new Error('useTranslatorConfigurer must be used within TranslationProvider.')
+    return configure as ConfigureFunction
+}
+
+export { useTranslate, useTranslatorConfigurer, TranslationProvider }
